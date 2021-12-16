a281d58741c9ea79695d593a10e1d20b1e8ab650
Cedric Cellier <rixed@happyleptic.org>
2021-12-16 11:48:10 +0100

    Add a scrollbar in alerts window
    Closes #61

--- src/alerting/AlertingWin.cpp
+++ src/alerting/AlertingWin.cpp
@@ -1,5 +1,6 @@
-#include <QWidget>
+#include <QScrollArea>
 #include <QVBoxLayout>
+#include <QWidget>
 
 #include "alerting/AlertingJournal.h"
 #include "alerting/AlertingLogsModel.h"
@@ -18,8 +19,20 @@ AlertingWin::AlertingWin(QWidget *parent)
   stats = new AlertingStats;
   layout->addWidget(stats);
 
+  /* Copied from StorageTimeLine which alludes to some issue if not wrapping into another
+   * widget: */
+  QScrollArea *scrollArea { new QScrollArea };
+  scrollArea->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+  scrollArea->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
+  scrollArea->setWidgetResizable(true);
+  QWidget *w { new QWidget };
+  QVBoxLayout *l1 { new QVBoxLayout };
+  w->setLayout(l1);
   timeLine = new AlertingTimeLine;
-  layout->addWidget(timeLine);
+  l1->addWidget(timeLine);
+  l1->setSizeConstraint(QLayout::SetMinimumSize);
+  scrollArea->setWidget(w);
+  layout->addWidget(scrollArea);
 
   journal = new AlertingJournal(AlertingLogsModel::globalLogsModel);
   layout->addWidget(journal);
