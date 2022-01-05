eacc5ef78396dbff7af3ba5e56687eb83a159f3a
Cedric Cellier <rixed@happyleptic.org>
2022-01-05 11:10:23 +0100

    Fix a mingw compilation issue (re. string compare)
    Closes #86

--- src/misc.cpp
+++ src/misc.cpp
@@ -31,12 +31,12 @@ QDebug operator<<(QDebug debug, uint128_t const &i) {
 
 bool startsWith(std::string const &a, std::string const &b) {
   if (a.length() < b.length()) return false;
-  return 0 == a.compare(0, b.length(), b, 0);
+  return 0 == a.compare(0, b.length(), b);
 }
 
 bool endsWith(std::string const &a, std::string const &b) {
   if (a.length() < b.length()) return false;
-  return 0 == a.compare(a.length() - b.length(), b.length(), b, 0);
+  return 0 == a.compare(a.length() - b.length(), b.length(), b);
 }
 
 std::string const removeExt(std::string const &s, char const c) {
